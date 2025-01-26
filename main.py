e9baba92506c790bfde48263ae35f4acd180c2b5
casmoth <tsuyoshi_nozaki@icloud.com>
2025-01-26 20:49:24 +0900

    make flie

---
+++ main.py
@@ -0,0 +1,45 @@
+# import area
+
+
+
+
+# 1
+# 入力 / input
+while True:
+    try:
+        link = str(input("リンク（例：https://www.ted.com/）："))
+        if link[0] == 'h':
+            break
+        else:
+            print("無効な入力です。")
+            print("-----------------------------------------------------")
+
+
+    except ValueError:
+        print("無効な入力です。")
+        print("-----------------------------------------------------")
+
+# 2
+while True:
+    try:
+        output_mode = int(input("出力するデータの型（txtデータなら1, Google docsなら2, wordなら3）："))
+        break
+
+    except ValueError:
+        print("無効な入力です。")
+        print("-----------------------------------------------------")
+    
+
+# 3
+
+
+
+# 4
+
+
+# 5
+# 5
+
+# 出力 / output
+# 6
+
